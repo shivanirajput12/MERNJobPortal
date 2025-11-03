40ee5ae6099ab55bbd8dec0219e39db685865dc3
shivani.rajput <shivani.rajput@officebeacon.com>
2025-11-03 13:39:23 +0530

    Initial commit for backend

---
+++ index.js
@@ -0,0 +1,31 @@
+// const express = require('express'); old way to require modules
+
+import express from 'express'; // new way using ES6 modules
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/home', (req, res)=>{
+   return res.status(200).json({message: "Welcome to Job Portal", succsess: true});
+});
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());    
+
+const corsOptions = {
+    origin: 'http://localhost:5173', // frontend URL
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}`);
+})
